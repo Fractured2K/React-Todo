13eb24e516f094bbe0273a7312663a73a4a8826e
Fractured2K <fractured2k@gmail.com>
2019-02-13 12:28:44 -0800

    Completed TodoForm Input

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const TodoForm = props => {
     return (
-        <div></div>
+        <div>
+            <form onSubmit={props.handleSubmit}>
+                <input
+                    type="text"
+                    name="todo"
+                    placeholder="Enter a todo"
+                    value={props.todo}
+                    onChange={props.handleChanges}
+                />
+            </form>
+        </div>
     )
 }
 
