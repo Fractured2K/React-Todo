40a965d791295e52085e4c97c24357a0b5dfd129
Fractured2K <fractured2k@gmail.com>
2019-02-13 12:21:42 -0800

    Imported all components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Todo from './components/TodoComponents/Todo';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+
 class App extends React.Component {
   constructor() {
     super()
